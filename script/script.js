8ec62678a05e16b9596e60c87f51bb753e8c63db
Sebastien34 <sebastienwendling@iteeweb.fr>
2023-12-05 23:02:33 +0100

    ajout bouton partager en JS

--- script/script.js
+++ script/script.js
@@ -29,6 +29,82 @@ function afficherProposition(proposition) {
   zoneProposition.innerText = proposition;
 }
 
+/**
+ * Cette fonction construit et affiche l'email.
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score.
+ */
+function afficherEmail(nom, email, score) {
+  let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`;
+  location.href = mailto;
+}
+
+/**
+ * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
+ * ici : deux caractères au minimum
+ * @param {string} nom
+ * @throws {Error}
+ */
+function validerNom(nom) {
+  if (nom.length < 2) {
+    throw new Error("Le nom est trop court. ");
+  }
+}
+
+/**
+ * Cette fonction prend un email en paramètre et valide qu'il est au bon format.
+ * @param {string} email
+ * @throws {Error}
+ */
+function validerEmail(email) {
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!emailRegExp.test(email)) {
+    throw new Error("L'email n'est pas valide.");
+  }
+}
+
+/**
+ * Cette fonction affiche le message d'erreur passé en paramètre.
+ * Si le span existe déjà, alors il est réutilisé pour ne pas multiplier
+ * les messages d'erreurs.
+ * @param {string} message
+ */
+function afficherMessageErreur(message) {
+  let spanErreurMessage = document.getElementById("erreurMessage");
+
+  if (!spanErreurMessage) {
+    let popup = document.querySelector(".popup");
+    spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
+
+    popup.append(spanErreurMessage);
+  }
+
+  spanErreurMessage.innerText = message;
+}
+
+/**
+ * Cette fonction permet de récupérer les informations dans le formulaire
+ * de la popup de partage et d'appeler l'affichage de l'email avec les bons paramètres.
+ * @param {string} scoreEmail
+ */
+function gererFormulaire(scoreEmail) {
+  try {
+    let baliseNom = document.getElementById("nom");
+    let nom = baliseNom.value;
+    validerNom(nom);
+
+    let baliseEmail = document.getElementById("email");
+    let email = baliseEmail.value;
+    validerEmail(email);
+    afficherMessageErreur("");
+    afficherEmail(nom, email, scoreEmail);
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message);
+  }
+}
+
 /*
  * Cette fonction lance le jeu.
  * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
